15c09620c5a51baac815f17cbc448121d7664979
Allen Mills <Allen_Mills@mail.fresnostate.edu>
2015-10-22 16:28:16 -0700

    Done! worked for far too long today but completed this version of allenmills.net

--- main.go
+++ main.go
@@ -12,8 +12,9 @@ var pages *template.Template
 func init() {
 	router := httprouter.New()
 	router.GET("/", showIndex)
-	router.GET("/portfolio", showPortfolio)
-	router.POST("/resume", showResume)
+	router.GET("/resume", showResume)
+
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public/"))))
 	http.Handle("/", router)
 	pages = template.Must(template.ParseFiles("index.html"))
 }
@@ -23,10 +24,6 @@ func showIndex(res http.ResponseWriter, req *http.Request, params httprouter.Par
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 	}
-}
-
-func showPortfolio(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
-	http.Redirect(res, req, "www.google.com", 302)
 }
 
 func showResume(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
