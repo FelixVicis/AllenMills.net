1d5bad5eec97fbf62e78d6e7e6709fe7f98ed9fb
Allen Mills <Allen_Mills@mail.fresnostate.edu>
2016-03-11 16:26:28 -0800

    [REFACTOR] A major update, we've gotten a job and have started tutoring. Site needs to reflect this.

--- main.go
+++ main.go
@@ -13,10 +13,14 @@ func init() {
 	router := httprouter.New()
 	router.GET("/", showIndex)
 	router.GET("/resume", showResume)
+	router.GET("/tutoring", showTutoring)
+	router.GET("/favicon.ico", favico)
 
 	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public/"))))
 	http.Handle("/", router)
-	pages = template.Must(template.ParseFiles("index.html"))
+
+	pages = template.Must(pages.ParseGlob("html/*.html"))
+	// pages = template.Must(template.ParseFiles("index.html"))
 }
 
 func showIndex(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
@@ -24,6 +28,17 @@ func showIndex(res http.ResponseWriter, req *http.Request, params httprouter.Par
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func showTutoring(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	err := pages.ExecuteTemplate(res, "tutoring.html", nil)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func favico(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	http.Redirect(res, req, "public/images/favicon.ico", 302)
 }
 
 func showResume(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
